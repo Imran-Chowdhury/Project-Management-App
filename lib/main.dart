e887df137a63d45542912cb2bbf7c44bba681cbe
Imran Chowdhury <imranchowdhury6093@gmail.com>
2024-10-15 18:24:01 +0600

    feat: Splash screen correctly implemented

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,6 @@ import 'dart:convert';
 
 import 'base_state/profile_state.dart';
 
-// Profile ViewModel Notifier provider
-// final profileViewModelNotifierProvider = StateNotifierProvider<ProfileViewModelNotifier, ProfileState>(
-//       (ref) => ProfileViewModelNotifier(restClient: RestClient()),
-// );
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,8 +37,10 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SafeArea(
-        child: AuthChecker(), // Check for authentication status on startup
+      home: const SafeArea(
+        child:
+        // SplashScreen(),
+        AuthChecker(), // Check for authentication status on startup
       ),
     );
   }
@@ -61,7 +59,8 @@ class AuthChecker extends ConsumerWidget {
       ),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator()); // Loading spinner
+          return  SplashScreen();
+            // const Center(child: CircularProgressIndicator()); // Loading spinner
         } else if (snapshot.hasData && ref.watch(profileViewModelProvider) is ProfileSuccessState) {
           // If the user is found, navigate to ProjectScreen
           return HomeScreen();
@@ -74,167 +73,3 @@ class AuthChecker extends ConsumerWidget {
     );
   }
 }
-
-
-
-
-
-//
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:project_management_app/views/getting_started_view.dart';
-//
-//
-// import 'package:project_management_app/views/home_view.dart';
-// import 'package:project_management_app/views/signin_view.dart';
-// import 'package:project_management_app/views/signup_view.dart';
-//
-//
-//
-//
-// Future main() async{
-//   WidgetsFlutterBinding.ensureInitialized();
-//   // await Firebase.initializeApp();
-//   runApp(
-//     const ProviderScope(child:  MyApp()),
-//
-//     );
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       // home: SafeArea(child: HomeScreen(),),
-//       // home: SafeArea(child: SignUpScreen(),),
-//       // home: SafeArea(child: SignInScreen(),),
-//       home: SafeArea(child: GettingStartedScreen(),),
-//
-//     );
-//   }
-// }
-//
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// // Define a task model
-// class Task {
-//   final String name;
-
-//   Task(this.name);
-// }
-
-// // Define a state class for tasks
-// class TaskState {
-//   final List<Task> tasks;
-
-//   TaskState(this.tasks);
-// }
-
-// // Define a state notifier for managing tasks
-// class TaskNotifier extends StateNotifier<TaskState> {
-//   TaskNotifier(List<Task> tasks) : super(TaskState(tasks));
-
-//   void addTask(Task task) {
-//     state = TaskState([...state.tasks, task]);
-//   }
-// }
-
-// // Define providers
-// final taskProvider = StateNotifierProvider<TaskNotifier, TaskState>((ref) {
-//   return TaskNotifier([]);
-// });
-
-// void main() {
-//   runApp(
-//     ProviderScope(
-//       child: MaterialApp(
-//         home: FirstScreen(),
-//       ),
-//     ),
-//   );
-// }
-
-// class FirstScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('First Screen'),
-//       ),
-//       body: ListView(
-//         children: [
-//           ListTile(
-//             title: Text('Eat'),
-//             onTap: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(
-//                   builder: (context) => SecondScreen(tasks: [Task('Eat')]),
-//                 ),
-//               );
-//             },
-//           ),
-//           ListTile(
-//             title: Text('Sleep'),
-//             onTap: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(
-//                   builder: (context) => SecondScreen(tasks: [Task('Sleep')]),
-//                 ),
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class SecondScreen extends ConsumerWidget {
-//   final List<Task> tasks;
-
-//   SecondScreen({required this.tasks});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final taskState = ref.watch(taskProvider);
-//     final taskController  = ref.watch(taskProvider.notifier);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Second Screen'),
-//       ),
-//       body: ListView.builder(
-//         itemCount: tasks.length,
-//         itemBuilder: (context, index) {
-//           return ListTile(
-//             title: Text(tasks[index].name),
-//           );
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           final newTask = Task('Walk');
-//           ref.watch(taskProvider.notifier).addTask(newTask);
-//         },
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-
-
-
-// }
--- lib/views/getting_started_view.dart
+++ lib/views/getting_started_view.dart
@@ -1,25 +1,22 @@
 
 import 'package:flutter/material.dart';
-import 'package:project_management_app/utils/validator.dart';
-import 'package:project_management_app/views/signin_view.dart';
-import 'package:project_management_app/views/signup_view.dart';
-import 'package:project_management_app/widgets/custom_button.dart';
-import 'package:project_management_app/widgets/custom_textfield.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  final _formKey = GlobalKey<FormState>();
+
 
   @override
   Widget build(BuildContext context) {
 
     final Size size = MediaQuery.of(context).size;
-    double width = size.width;
+
     double height = size.height;
 
     return Scaffold(
@@ -34,6 +31,8 @@ class _SplashScreenState extends State<SplashScreen> {
              height: height*.5, // Make sure the image covers the whole screen
             ),
           ),
+
+          // const AuthChecker(),
 
         ],
       ),
