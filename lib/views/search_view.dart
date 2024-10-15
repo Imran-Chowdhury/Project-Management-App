90e8f1c1cbd77d7e3ce01dac2cbaa6559b55f043
Imran Chowdhury <imranchowdhury6093@gmail.com>
2024-10-15 21:17:54 +0600

    feat: search screen ui updated

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -7,105 +7,6 @@ import '../utils/search_result_widget.dart';
 
 import '../view_models/search_view_model.dart';
 
-// class SearchScreen extends ConsumerStatefulWidget {
-//   @override
-//   _SearchScreenState createState() => _SearchScreenState();
-//   SearchScreen({super.key, required this.allProjects, required this.accessToken});
-//   List<Project> allProjects;
-//   String accessToken;
-// }
-//
-// class _SearchScreenState extends ConsumerState<SearchScreen> {
-//   TextEditingController searchController = TextEditingController();
-//
-//
-//
-//   @override
-//   void dispose() {
-//     searchController.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final filteredProjects = ref.watch(projectProvider);
-//     SearchNotifier controller = ref.watch(projectProvider.notifier);
-//     final Size size = MediaQuery. of(context).size;
-//     double width = size.width;
-//     double height = size.height;
-//
-//     return Scaffold(
-//       appBar:AppBar(
-//         title: const Text('Search Projects',
-//           style: TextStyle(fontWeight: FontWeight.bold),),
-//         backgroundColor: const Color(0XFFD3D3D3),
-//       ),
-//       body: Column(
-//         children: [
-//
-//          Padding(
-//            padding: EdgeInsets.only(left: width*.17,top: height*.01),
-//            child: Image.asset(
-//                 'assets/images/login.png', // Your image path
-//                 height: height * 0.3,
-//               ),
-//          ),
-//
-//           TextFormField(
-//             onChanged: (value){
-//               controller.filterProjects(value, widget.allProjects);
-//             },
-//             controller: searchController,
-//             decoration:const  InputDecoration(
-//               labelText: 'Search',
-//               hintText: 'Enter project name',
-//               border:  OutlineInputBorder(),
-//             ),
-//
-//           ),
-//
-//
-//
-//
-//
-//           Stack(
-//           children: [
-//
-//             Padding(
-//               padding: EdgeInsets.only(top: height*0.4),
-//               child: Container(
-//                 decoration: const BoxDecoration(
-//                   borderRadius: BorderRadius.only(topLeft:Radius.circular(40),topRight: Radius.circular(40)),
-//                   // color: Color(0XFFffffff),
-//                   color: Color(0XFFffda21),
-//                 ),
-//                 width: double.infinity,
-//                 height: height*0.9,
-//
-//               ),
-//             ),
-//
-//             SizedBox(height: height*.05,),
-//
-//
-//
-//             const SizedBox(height: 20),
-//             Expanded(
-//               child: ListView.builder(
-//                 itemCount: filteredProjects.length,
-//                 itemBuilder: (context, index) {
-//                   return searchResultTile(filteredProjects, index, context, widget.accessToken);
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 
 class SearchScreen extends ConsumerStatefulWidget {
   @override
@@ -133,6 +34,7 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
     double height = size.height;
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: const Color(0XFFffffff),
       appBar: AppBar(
         title: const Text(
@@ -153,7 +55,7 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
           ),
 
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.fromLTRB(30,20,30,16),
             child: TextFormField(
               onChanged: (value) {
                 controller.filterProjects(value, widget.allProjects);
@@ -162,26 +64,22 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
               decoration: const InputDecoration(
                 labelText: 'Search',
                 hintText: 'Enter project name',
+
                 border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Color(0XFFD3D3D3),
+                prefixIcon: Icon(Icons.search_sharp),
               ),
             ),
           ),
 
-         // filteredProjects.isEmpty? const Text(
-         //   'Search for Projects',
-         //   style: TextStyle(
-         //       color: Colors.black45,
-         //       fontWeight: FontWeight.bold,
-         //       fontSize: 20
-         //
-         //   ),
-         // ): SizedBox(),
-          Expanded(
+
+         filteredProjects.isNotEmpty? Expanded(
             child: Stack(
               children: [
                 // Yellow background container
                 Positioned(
-                  top: height * 0.08, // Adjust the height to start the background higher
+                  top: height * 0.05, // Adjust the height to start the background higher
                   child: Container(
                     decoration: const BoxDecoration(
                       borderRadius: BorderRadius.only(
@@ -194,6 +92,8 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
                     height: height * 0.75, // Cover most of the height for the container
                   ),
                 ),
+
+
                 // ListView inside the yellow container
                 Padding(
                   padding: EdgeInsets.only(top: height * 0.01), // Adjust the top padding to position ListView
@@ -206,7 +106,7 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
                 ),
               ],
             ),
-          ),
+          ):SizedBox(),
         ],
       ),
     );
